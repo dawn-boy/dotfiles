22bf7a60829d652965d6abac6c2b0cf83dac4a0c
dawn-boy <curiouslittleboy@pm.me>
2025-02-04 23:30:59 +0530

    updated fish path for custom commands

--- qtile/AspectLook/config/fish/config.fish
+++ qtile/AspectLook/config/fish/config.fish
@@ -5,6 +5,6 @@ if status is-interactive
 	starship init fish | source
 	cat ~/.cache/wal/sequences &
 	set fish_greeting
-	set -U fish_user_paths ~/.github/cmds/ $fish_user_paths
+	set -U fish_user_paths ~/.github/scripts/cmds $fish_user_paths
 end
 
